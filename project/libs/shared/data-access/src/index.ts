6d65f3ab57fc4114b3507101e9068a4ccaec4749
Дубровин Василий <thebestmane@outlook.com>
2025-05-04 15:45:08 +0400

    Добавит day-js, импортирует blog-user модуль в authentication модуль.

--- project/libs/shared/data-access/src/index.ts
+++ project/libs/shared/data-access/src/index.ts
@@ -1,1 +1,1 @@
-export * from './lib/data-access.js';
+export { BaseMemoryRepository }  from './repository/base-memory.repository.js';
--- project/libs/user/blog-user/src/blog-user-module/blog-user.module.ts
+++ project/libs/user/blog-user/src/blog-user-module/blog-user.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { BlogUserController } from './blog-user.controller';
 import { BlogUserService } from './blog-user.service';
+import { BlogUserRepository } from './blog-user.repository.js';
+import { BlogUserFactory } from './blog-user.factory.js';
 
 @Module({
   controllers: [BlogUserController],
-  providers: [BlogUserService],
+  providers: [
+    BlogUserRepository,
+     BlogUserService,
+    BlogUserFactory,
+  ],
+  exports: [BlogUserRepository]
 })
 export class BlogUserModule {}
